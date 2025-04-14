4a795a94b0c4ac2095e7ca6719650c74bd2c75c6
Adam Viaud <adam.viaud@proton.ch>
2025-04-14 14:50:32 +0200

    feat: Hermes introduction

--- Sources/WireGuardKit/DNSServer.swift
+++ Sources/WireGuardKit/DNSServer.swift
@@ -4,30 +4,73 @@
 import Foundation
 import Network
 
+public enum DNSTransport {
+    case classic
+    case https
+    case tls
+}
+
+extension DNSTransport {
+    static func transport(from stringValue: some StringProtocol) -> DNSTransport? {
+        switch stringValue {
+        case "https":
+            return .https
+        case "tls":
+            return .tls
+        default:
+            return nil
+        }
+    }
+}
+
 public struct DNSServer {
     public let address: IPAddress
+    public let transport: DNSTransport
 
-    public init(address: IPAddress) {
+    public init(address: IPAddress, transport: DNSTransport) {
         self.address = address
+        self.transport = transport
     }
 }
 
 extension DNSServer: Equatable {
     public static func == (lhs: DNSServer, rhs: DNSServer) -> Bool {
-        return lhs.address.rawValue == rhs.address.rawValue
+        return lhs.address.rawValue == rhs.address.rawValue && lhs.transport == rhs.transport
     }
 }
 
 extension DNSServer {
     public var stringRepresentation: String {
-        return "\(address)"
+        switch transport {
+        case .https:
+            return "https://\(address)/dns-query"
+        case .tls, .classic:
+            return "\(address)"
+        }
     }
 
     public init?(from addressString: String) {
-        if let addr = IPv4Address(addressString) {
-            address = addr
-        } else if let addr = IPv6Address(addressString) {
-            address = addr
+        let host: String
+        let transport: DNSTransport
+        if #available(iOS 16.0, macOS 13.0, *) {
+            let components = addressString.split(separator: "://")
+            if components.count > 1 {
+                host = String(components[1])
+                transport = DNSTransport.transport(from: components[0]) ?? .classic
+            } else {
+                host = String(components[0])
+                transport = .classic
+            }
+        } else {
+            host = addressString
+            transport = .classic
+        }
+        if let addr = IPv4Address(host) {
+            self.address = addr
+            self.transport = transport
+        } else if let addr = IPv6Address(host) {
+            self.address = addr
+            self.transport = transport
         } else {
             return nil
         }
--- Sources/WireGuardKit/PacketTunnelSettingsGenerator.swift
+++ Sources/WireGuardKit/PacketTunnelSettingsGenerator.swift
@@ -12,7 +12,7 @@ import WireGuardKitC
 /// A type alias for `Result` type that holds a tuple with source and resolved endpoint.
 typealias EndpointResolutionResult = Result<(Endpoint, Endpoint), DNSResolutionError>
 
-class PacketTunnelSettingsGenerator {
+final class PacketTunnelSettingsGenerator {
     let tunnelConfiguration: TunnelConfiguration
     let resolvedEndpoints: [Endpoint?]
 
@@ -74,6 +74,53 @@ class PacketTunnelSettingsGenerator {
         return (wgSettings, resolutionResults)
     }
 
+    func appropriateDNSSettings() -> NEDNSSettings {
+        // When using DoH/DoT, it seems we can only set one serverName/serverURL
+        let firstElement = tunnelConfiguration.interface.dns[0]
+        switch firstElement.transport {
+        case .tls:
+            if #available(iOS 14.0, macOS 11.0, *) {
+                let dnsSettings = NEDNSOverTLSSettings()
+                dnsSettings.serverName = firstElement.stringRepresentation
+                return dnsSettings
+            } else {
+                fallthrough
+            }
+        case .https:
+            if #available(iOS 14.0, macOS 11.0, *) {
+                let dnsSettings = NEDNSOverHTTPSSettings()
+                dnsSettings.serverURL = URL(string: firstElement.stringRepresentation)!
+                return dnsSettings
+            } else {
+                fallthrough
+            }
+        case .classic:
+            let servers: [String]
+            if case .classic = firstElement.transport {
+                servers = tunnelConfiguration.interface.dns.map { $0.stringRepresentation }
+            } else {
+                servers = tunnelConfiguration.interface.dns[1...].map { $0.stringRepresentation }
+            }
+            return NEDNSSettings(servers: servers)
+        }
+    }
+
+    func setDNSSettings(to networkSettings: NEPacketTunnelNetworkSettings) {
+        guard !tunnelConfiguration.interface.dnsSearch.isEmpty || !tunnelConfiguration.interface.dns.isEmpty else {
+            return
+        }
+
+        let dnsSettings: NEDNSSettings = appropriateDNSSettings()
+
+        if !tunnelConfiguration.interface.dns.isEmpty {
+            dnsSettings.matchDomains = [""] // All DNS queries must first go through the tunnel's DNS
+        }
+
+        dnsSettings.searchDomains = tunnelConfiguration.interface.dnsSearch
+
+        networkSettings.dnsSettings = dnsSettings
+    }
+
     func generateNetworkSettings() -> NEPacketTunnelNetworkSettings {
         /* iOS requires a tunnel endpoint, whereas in WireGuard it's valid for
          * a tunnel to have no endpoint, or for there to be many endpoints, in
@@ -83,15 +130,7 @@ class PacketTunnelSettingsGenerator {
          */
         let networkSettings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "127.0.0.1")
 
-        if !tunnelConfiguration.interface.dnsSearch.isEmpty || !tunnelConfiguration.interface.dns.isEmpty {
-            let dnsServerStrings = tunnelConfiguration.interface.dns.map { $0.stringRepresentation }
-            let dnsSettings = NEDNSSettings(servers: dnsServerStrings)
-            dnsSettings.searchDomains = tunnelConfiguration.interface.dnsSearch
-            if !tunnelConfiguration.interface.dns.isEmpty {
-                dnsSettings.matchDomains = [""] // All DNS queries must first go through the tunnel's DNS
-            }
-            networkSettings.dnsSettings = dnsSettings
-        }
+        setDNSSettings(to: networkSettings)
 
         let mtu = tunnelConfiguration.interface.mtu ?? 0
 
